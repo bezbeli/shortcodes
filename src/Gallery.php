6a9274cd3c299b4b817f16f01fa3446022e76caf
Enes Huseinčehajić <bezbeli@gmail.com>
2018-08-14 19:14:29 +0200

    add new blueimp

--- src/Gallery.php
+++ src/Gallery.php
@@ -1,15 +1,19 @@
 <?php
 
-// nova blueimp galerija
-
 namespace Bezbeli\Gallery;
 
 class Gallery
 {
+    public function __construct()
+    {
+        remove_shortcode('gallery');
+        add_shortcode('gallery', __NAMESPACE__.'\\blueimp_gallery');
+    }
+
     public function blueimpGallery($attr)
     {
         static $instance = 0;
-        $instance++;
+        ++$instance;
 
         $id = get_the_id();
         $columns = $attr['columns'];
@@ -18,23 +22,23 @@ class Gallery
         $orderby = $attr['orderby'];
         $link = '';
 
-        $columns = (12 % $columns == 0) ? $columns : 3;
+        $columns = (0 == 12 % $columns) ? $columns : 3;
         $grid = sprintf('col-%1$s col-sm-%1$s col-md-%1$s', 12 / $columns);
 
         $attachments = get_children([
-                'post_parent'       => $id,
-                'post_status'       => 'inherit',
-                'post_type'         => 'attachment',
-                'post_mime_type'    => 'image',
-                'orderby'           => $orderby,
-                'order'             => $order,
+                'post_parent' => $id,
+                'post_status' => 'inherit',
+                'post_type' => 'attachment',
+                'post_mime_type' => 'image',
+                'orderby' => $orderby,
+                'order' => $order,
             ]);
 
         if (empty($attachments)) {
             return '';
         }
 
-        $unique = (get_query_var('page')) ? $instance . '-p' . get_query_var('page') : $instance;
+        $unique = (get_query_var('page')) ? $instance.'-p'.get_query_var('page') : $instance;
         $output = '';
 
         /*
@@ -45,35 +49,16 @@ class Gallery
 
         // if ($unique == 1) {
         $output .= '
-                    <div id="blueimp-gallery" class="blueimp-gallery" data-use-bootstrap-modal="false">
-                        <div class="slides"></div>
-                        <h3 class="title"></h3>
-                        <a class="prev">‹</a>
-                        <a class="next">›</a>
-                        <a class="close">×</a>
-                        <a class="play-pause"></a>
-                        <div class="modal fade">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <button type="button" class="close" aria-hidden="true">&times;</button>
-                                        <h4 class="modal-title"></h4>
-                                    </div>
-                                    <div class="modal-body next"></div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-default pull-left prev">
-                                            <i class="glyphicon glyphicon-chevron-left"></i>
-                                            Previous
-                                        </button>
-                                        <button type="button" class="btn btn-primary next">
-                                            Next
-                                            <i class="glyphicon glyphicon-chevron-right"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>';
+        <div id="blueimp-gallery" class="blueimp-gallery">
+            <div class="slides"></div>
+            <h3 class="title"></h3>
+            <a class="prev">‹</a>
+            <a class="next">›</a>
+            <a class="close">×</a>
+            <a class="play-pause"></a>
+            <ol class="indicator"></ol>
+        </div>
+        ';
         // }
         $output .= '<div id="links" class="gallery">';
 
@@ -88,25 +73,25 @@ class Gallery
                     $image = wp_get_attachment_image($id, $size, false, ['class' => 'thumbnail img-thumbnail']);
                     break;
                 default:
-                    $image = '<a href="' . wp_get_attachment_image_src($id, 'large')[0] . '" title="'.$attachment->post_title.'" data-gallery data-description="'.$attachment->post_excerpt.'">';
+                    $image = '<a href="'.wp_get_attachment_image_src($id, 'large')[0].'" title="'.$attachment->post_title.'" data-gallery data-description="'.$attachment->post_excerpt.'">';
                     $image .= wp_get_attachment_image($id, $size, false, ['class' => 'thumbnail img-thumbnail']);
                     $image .= '</a>';
                     break;
             }
 
-            $output .= ($i % $columns == 0) ? '<div class="row justify-content-center">': '';
-            $output .= '<div class="' . $grid .' mb-4">' . $image;
+            $output .= (0 == $i % $columns) ? '<div class="row justify-content-center">' : '';
+            $output .= '<div class="'.$grid.' mb-4">'.$image;
 
             if (trim($attachment->post_excerpt)) {
-                $output .= '<div class="caption hidden">' . wptexturize($attachment->post_excerpt) . '</div>';
+                $output .= '<div class="caption hidden">'.wptexturize($attachment->post_excerpt).'</div>';
             }
 
             $output .= '</div>';
-            $i++;
-            $output .= ($i % $columns == 0) ? '</div>' : '';
+            ++$i;
+            $output .= (0 == $i % $columns) ? '</div>' : '';
         }
 
-        $output .= ($i % $columns != 0) ? '</div>' : '';
+        $output .= (0 != $i % $columns) ? '</div>' : '';
         $output .= '</div>';
 
         return $output;
