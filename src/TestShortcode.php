2519bd7dfb6c63cc40e718a5a40a0603eee61b4e
Enes Huseinčehajić <bezbeli@gmail.com>
2018-08-15 09:55:00 +0200

    add src folder

---
+++ src/TestShortcode.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bezbeli;
+
+/**
+ * Test shortcode class.
+ */
+class TestShortcode
+{
+    /** Render shortcode method
+     **/
+    public function render()
+    {
+        return 'dva';
+    }
+}
